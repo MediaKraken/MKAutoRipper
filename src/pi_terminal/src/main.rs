81c36411c75a208b34e0f845841b27e35bd8cec0
spootdev <spootdev@gmail.com>
2023-10-11 20:03:47 -0500

    positions

--- src/pi_terminal/src/main.rs
+++ src/pi_terminal/src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     let app = app::App::default();
     let mut win = Window::default().with_size(800, 480);
 
-    let mut container_spindle = Pack::new(150, 150, 200, 100, "CounterApplet ");
+    let mut container_spindle = Pack::new(10, 10, 250, 35, "Spindle Type");
 
     // setup control for spindle media
     let mut choice_spindle_1_media_type = choice::MyChoice::new(20, 20, 80, 30, None);
     choice_spindle_1_media_type.add_choices(&["None", "CD", "DVD", "BRAY", "UHD", "HDDVD"]);
-    choice_spindle_1_media_type.set_current_choice(1);
+    choice_spindle_1_media_type.set_current_choice(0);
     choice_spindle_1_media_type
         .button()
         .set_frame(FrameType::BorderBox);
@@ -21,7 +21,7 @@ fn main() {
     // setup control for spindle media
     let mut choice_spindle_2_media_type = choice::MyChoice::new(20, 120, 80, 30, None);
     choice_spindle_2_media_type.add_choices(&["None", "CD", "DVD", "BRAY", "UHD", "HDDVD"]);
-    choice_spindle_2_media_type.set_current_choice(1);
+    choice_spindle_2_media_type.set_current_choice(0);
     choice_spindle_2_media_type
         .button()
         .set_frame(FrameType::BorderBox);
@@ -32,7 +32,7 @@ fn main() {
     // setup control for spindle media
     let mut choice_spindle_3_media_type = choice::MyChoice::new(20, 220, 80, 30, None);
     choice_spindle_3_media_type.add_choices(&["None", "CD", "DVD", "BRAY", "UHD", "HDDVD"]);
-    choice_spindle_3_media_type.set_current_choice(1);
+    choice_spindle_3_media_type.set_current_choice(0);
     choice_spindle_3_media_type
         .button()
         .set_frame(FrameType::BorderBox);
@@ -43,7 +43,7 @@ fn main() {
     // setup control for spindle media
     let mut choice_spindle_4_media_type = choice::MyChoice::new(20, 320, 80, 30, None);
     choice_spindle_4_media_type.add_choices(&["None", "CD", "DVD", "BRAY", "UHD", "HDDVD"]);
-    choice_spindle_4_media_type.set_current_choice(1);
+    choice_spindle_4_media_type.set_current_choice(0);
     choice_spindle_4_media_type
         .button()
         .set_frame(FrameType::BorderBox);
@@ -53,10 +53,19 @@ fn main() {
 
     container_spindle.end();
     container_spindle.set_frame(FrameType::BorderFrame);
-    container_spindle.set_color(Color::Red);
+    container_spindle.set_color(Color::Black);
     container_spindle.set_type(PackType::Horizontal);
 
-    let mut button_rip = Button::new(160, 210, 80, 40, "Start Ripping!");
+    let mut container_status = Pack::new(10, 210, 250, 350, "Status");
+
+    container_status.end();
+    container_status.set_frame(FrameType::BorderFrame);
+    container_status.set_color(Color::Black);
+    container_status.set_type(PackType::Horizontal);
+
+    let mut button_rip = Button::new(560, 210, 120, 40, "Start Ripping!");
+
+    let mut button_stop = Button::new(600, 310, 120, 40, "Stop!");
 
     win.end();
     win.show();
