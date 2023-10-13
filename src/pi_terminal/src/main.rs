7d0ee2d0567982fee272a62275a4765db0c58448
spootdev <spootdev@gmail.com>
2023-10-13 12:34:28 -0500

    info stats

--- src/pi_terminal/src/main.rs
+++ src/pi_terminal/src/main.rs
@@ -1,11 +1,19 @@
 use fltk::{app, app::*, button::*, enums::*, frame::*, group::*, prelude::*, window::*};
+use fltk_table::{SmartTable, TableOpts};
+use std::fs;
+use std::{cell::RefCell, rc::Rc};
+
 mod choice;
 
 fn main() {
     let app = app::App::default();
+
+    let position_horizontal = Rc::new(RefCell::new(0));
+    let position_vertical = Rc::new(RefCell::new(0));
+
     let mut win = Window::default().with_size(800, 480);
 
-    let mut container_spindle = Pack::new(20, 10, 300, 35, "Spindle Type");
+    let mut container_spindle = Pack::new(10, 25, 300, 35, "Spindle Type");
 
     // setup control for spindle media
     let mut choice_spindle_1_media_type = choice::MyChoice::new(20, 20, 80, 30, None);
@@ -56,18 +64,154 @@ fn main() {
     container_spindle.set_color(Color::Black);
     container_spindle.set_type(PackType::Horizontal);
 
-    let mut container_status = Pack::new(10, 210, 250, 350, "Status");
+    let mut container_status = Pack::new(10, 90, 300, 375, "Status");
+
+    let mut status_table = SmartTable::default()
+        .with_size(300, 375)
+        .center_of_parent()
+        .with_opts(TableOpts {
+            rows: 6,
+            cols: 3,
+            editable: false,
+            ..Default::default()
+        });
 
     container_status.end();
     container_status.set_frame(FrameType::BorderFrame);
     container_status.set_color(Color::Black);
     container_status.set_type(PackType::Horizontal);
 
-    let mut button_rip = Button::new(600, 210, 120, 40, "Start Ripping!");
+    let mut container_info = Pack::new(325, 90, 250, 250, "Info");
 
-    let mut button_stop = Button::new(600, 310, 120, 40, "Stop!");
+    let mut info_table = SmartTable::default()
+        .with_size(250, 350)
+        .center_of_parent()
+        .with_opts(TableOpts {
+            rows: 12,
+            cols: 2,
+            editable: false,
+            ..Default::default()
+        });
+    info_table.set_cell_value(0, 0, "Model");
+    let pi_model = fs::read_to_string("/sys/firmware/devicetree/base/model").unwrap();
+    info_table.set_cell_value(0, 1, &pi_model);
+    info_table.set_cell_value(1, 0, "Memory");
+    // pinout exe
+    info_table.set_cell_value(1, 1, "N/A");
+    info_table.set_cell_value(2, 0, "Disk");
+    info_table.set_cell_value(2, 1, "N/A");
+    info_table.set_cell_value(3, 0, "Camera");
+    info_table.set_cell_value(3, 1, "N/A");
+    info_table.set_cell_value(4, 0, "OS");
+    let pi_os = fs::read_to_string("/proc/version").unwrap();
+    info_table.set_cell_value(4, 1, &pi_os);
+
+    container_info.end();
+    container_info.set_frame(FrameType::BorderFrame);
+    container_info.set_color(Color::Black);
+    container_info.set_type(PackType::Horizontal);
+
+    let mut container_position = Pack::new(625, 25, 150, 40, "Position - step(s)");
+
+    let mut frame_position_horizontal = Frame::default().with_size(40, 20).with_label("Horiz: 0");
+    let mut frame_position_vertical = Frame::default().with_size(40, 20).with_label("Vert: 0");
+
+    container_position.end();
+    container_position.set_frame(FrameType::BorderFrame);
+    container_position.set_color(Color::Black);
+    container_position.set_type(PackType::Vertical);
+
+    // move the arms around
+    let mut button_zero = Button::new(400, 15, 150, 50, "Zero Everything");
+    let mut button_left = Button::new(625, 100, 50, 50, "Left");
+    let mut button_up = Button::new(675, 75, 50, 50, "Up");
+    let mut button_down = Button::new(675, 125, 50, 50, "Down");
+    let mut button_right = Button::new(725, 100, 50, 50, "Right");
+    // activate equipment
+    let mut button_vacuum = Button::new(620, 180, 80, 50, "Vacuum");
+    let mut button_snapshot = Button::new(700, 180, 80, 50, "Snapshot");
+    // following are for buffer/cleaner
+    let mut button_water = Button::new(585, 240, 70, 50, "Water");
+    let mut button_spinner = Button::new(655, 240, 70, 50, "Spinner");
+    let mut button_buffer = Button::new(725, 240, 70, 50, "Buffer");
+
+    // start/stop ripping
+    let mut button_start = Button::new(500, 420, 150, 60, "Start Ripping!");
+    let mut button_stop = Button::new(650, 420, 150, 60, "Stop!");
 
     win.end();
     win.show();
+
+    // position button click processing
+    button_right.set_callback({
+        let position_horizontal = position_horizontal.clone();
+        let mut frame_position_horizontal = frame_position_horizontal.clone();
+        move |_| {
+            *position_horizontal.borrow_mut() += 1;
+            frame_position_horizontal.set_label(&format!(
+                "Horiz: {}",
+                &position_horizontal.borrow().to_string()
+            ));
+        }
+        // TODO move arm
+    });
+
+    button_left.set_callback(move |_| {
+        *position_horizontal.borrow_mut() -= 1;
+        frame_position_horizontal.set_label(&format!("Horiz: {}", &position_horizontal.borrow()));
+        // TODO move arm
+    });
+
+    button_up.set_callback({
+        let position_vertical = position_vertical.clone();
+        let mut frame_position_vertical = frame_position_vertical.clone();
+        move |_| {
+            *position_vertical.borrow_mut() += 1;
+            frame_position_vertical.set_label(&format!(
+                "Vert: {}",
+                &position_vertical.borrow().to_string()
+            ));
+        }
+        // TODO move arm
+    });
+
+    button_down.set_callback(move |_| {
+        *position_vertical.borrow_mut() -= 1;
+        frame_position_vertical.set_label(&format!("Vert: {}", &position_vertical.borrow()));
+        // TODO move arm
+    });
+
+    button_vacuum.set_callback(move |_| {
+        // TODO toggle vacuum
+    });
+
+    button_snapshot.set_callback(move |_| {
+        // TODO take image via raspberry pi camera
+    });
+
+    button_water.set_callback(move |_| {
+        // TODO toggle water flow
+    });
+
+    button_spinner.set_callback(move |_| {
+        // TODO toggle media spinner motor
+    });
+
+    button_buffer.set_callback(move |_| {
+        // TODO toggle cleaner/buffer motor
+    });
+
+    button_zero.set_callback(move |_| {
+        // TODO move everything to zero
+    });
+
+    button_start.set_callback(move |_| {
+        // TODO start ripping media
+    });
+
+    button_stop.set_callback(move |_| {
+        // TODO stop the system immediately
+    });
+
     app.run().unwrap();
 }
