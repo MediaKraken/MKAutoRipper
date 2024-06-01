8d3b1f6b49f597037894c8060afd15768923338a
spootdev <spootdev@gmail.com>
2024-06-01 12:47:45 -0500

    deps

--- models/readme.md
+++ models/readme.md
@@ -1,1 +1,6 @@
 Fan__60mm.STL is by leaftye (https://www.thingiverse.com/leaftye) is licensed under the Creative Commons - Attribution license
+
+https://www.thingiverse.com/thing:44190
+https://www.thingiverse.com/thing:1422691
+
+https://www.thingiverse.com/thing:3444382
--- src/docker/ripper/src/main.rs
+++ src/docker/ripper/src/main.rs
@@ -1,12 +1,13 @@
 mod drive_commands;
 use serde_json::Value;
 use std::error::Error;
-use std::process::{Command, Stdio};
+use std::process::{exit, Command, Stdio};
+use sysinfo::{Components, Disks, Networks, Pid, Signal, System, Users};
 use tokio::sync::mpsc::UnboundedReceiver;
 use tokio::sync::Notify;
 use tokio::time::{sleep, Duration};
-
 mod rabbit;
+use std::process;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -20,6 +21,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
             if let Some(payload) = msg.content {
                 let json_message: Value =
                     serde_json::from_str(&String::from_utf8_lossy(&payload)).unwrap();
+                let mut exit_container: bool = false;
                 // perform command sent to ripper
                 if json_message["Type"] == "makemkv" {
                     // begin makemkv rip
@@ -38,9 +40,37 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     let stdout = String::from_utf8(output.stdout).unwrap();
                     let json_output: Value = serde_json::from_str(&stdout).unwrap();
                 } else if json_message["Type"] == "abcde" {
-                    // TODO begin abcde rip
+                    // begin abcde rip
+                    let output = Command::new("abcde")
+                        .args([
+                            "-d",
+                            "/dev/cdrom",
+                            "/ripoutput/.",
+                        ])
+                        .stdout(Stdio::piped())
+                        .output()
+                        .unwrap();
+                    let stdout = String::from_utf8(output.stdout).unwrap();
+                    let json_output: Value = serde_json::from_str(&stdout).unwrap();
+                    // TODO look for "Finished."
                 } else if json_message["Type"] == "stop" {
-                    // TODO stop rip
+                    // stop rip
+                    let s = System::new_all();
+                    for process in s.processes_by_name("makemkvcon") {
+                        if let Some(process) = s.process(process.pid()) {
+                            if process.kill_with(Signal::Kill).is_none() {
+                                println!("This signal isn't supported on this platform");
+                            }
+                        }
+                    }
+                    for process in s.processes_by_name("abcde") {
+                        if let Some(process) = s.process(process.pid()) {
+                            if process.kill_with(Signal::Kill).is_none() {
+                                println!("This signal isn't supported on this platform");
+                            }
+                        }
+                    }
+                    exit_container = true;
                 } else if json_message["Type"] == "eject" {
                     // eject media
                     let _results = drive_commands::drive_eject("/dev/cdrom").await;
@@ -49,6 +79,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 let _result =
                     rabbit::rabbitmq_ack(&rabbit_channel, msg.deliver.unwrap().delivery_tag())
                         .await;
+                if exit_container == true {
+                    // Exit program and stop container
+                    process::exit(0x0100);
+                }
             }
             sleep(Duration::from_secs(1)).await;
         }
