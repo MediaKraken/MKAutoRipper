b45e8ea83b3b38d3a4a5321d454fb8dc0a70a0cd
spootdev <spootdev@gmail.com>
2025-01-19 18:36:15 -0600

    setup pin

--- src/gpio_pin_state_checker.py
+++ src/gpio_pin_state_checker.py
@@ -3,6 +3,9 @@ GPIO.setmode(GPIO.BCM)
 GPIOs = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
          12, 13, 16, 17, 18, 19, 20, 21,
          22, 23, 24, 25, 26, 27]
+# Setup all GPIOs to input
+for gpio in GPIOs:
+    GPIO.setup(gpio, GPIO.IN)
     
 # Read state for each GPIO
 for gpio in GPIOs:
