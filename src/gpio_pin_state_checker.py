3aaac9a651bd1e3955992c35ba67ef0ca122b394
spootdev <spootdev@gmail.com>
2025-01-19 18:35:13 -0600

    pin test

---
+++ src/gpio_pin_state_checker.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIOs = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+         12, 13, 16, 17, 18, 19, 20, 21,
+         22, 23, 24, 25, 26, 27]
+    
+# Read state for each GPIO
+for gpio in GPIOs:
+    print("GPIO no " + str(gpio) + ": " + str(GPIO.input(gpio)))
+    
+# Cleanup all GPIOs - state will be different after that!
+GPIO.cleanup()
